b3fbe1ac13c3e4c99b5de9546a85ee764433338a
a.shakhovnina <a.shakhovnina@tinkoff.ru>
2021-03-14 22:46:42 +0300

    add TestGetBooking

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -1,4 +1,4 @@
-"""Проверка запроса на создание бронирования"""
+"""Проверка запроса на создание бронирования."""
 from model.booking import BookingData
 import pytest
 
@@ -6,7 +6,7 @@ import pytest
 class TestCreateBooking:
     def test_create_booking(self, client):
         """
-        Отправка запроса 'create_booking' с рандомными валидными данными
+        Отправка запроса 'create_booking' с рандомными валидными данными.
         """
         data = BookingData().random()
         response = client.booking.create_booking(data)
@@ -17,7 +17,7 @@ class TestCreateBooking:
     @pytest.mark.parametrize('value',
                              [1, 0, True, False])
     def test_depositpaid(self, client, value):
-        """Проверка допустимых значений параметра depositpaid"""
+        """Проверка допустимых значений параметра depositpaid."""
         data = BookingData().random()
         setattr(data, 'depositpaid', value)
         response = client.booking.create_booking(data)
@@ -28,7 +28,7 @@ class TestCreateBooking:
     @pytest.mark.parametrize("value",
                              [1, 1000, 1000000, 0, 1])
     def test_total_price(self, client, value):
-        """Проверка допустимых значений параметра totalprice"""
+        """Проверка допустимых значений параметра totalprice."""
         data = BookingData().random()
         setattr(data, 'totalprice', value)
         response = client.booking.create_booking(data)
---
+++ tests/test_get_booking.py
@@ -0,0 +1,19 @@
+"""Получение запроса по id."""
+from model.booking import BookingData
+
+
+class TestGetBooking:
+    def test_get_booking(self, client):
+        """
+        Запрос с id=1 всегда существует в системе, поэтому ищём его.
+        """
+        response = client.booking.get_booking(1)
+        assert response.status_code == 200, "Запрос не найден"
+
+    def test_get_unexisting_booking(self, client):
+        """
+        Поиск по несуществующему id
+        """
+        response = client.booking.get_booking("euhls")
+        assert response.status_code == 404 and response.text == "Not Found"
+
